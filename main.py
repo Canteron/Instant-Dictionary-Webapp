87744b2396947b92462db309b8ec70d8fbeac112
BioRoi <roitorrente@hotmail.com>
2023-01-19 15:39:20 +0100

    App terminada

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import inspect
 import justpy as jp
 
 from webapp import page
-from webapp.about import About
-from webapp.dictionary import Dictionary
-from webapp.home import Home
+# from webapp.about import About
+# from webapp.dictionary import Dictionary
+# from webapp.home import Home
 
 
 
